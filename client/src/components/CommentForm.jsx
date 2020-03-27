80ca63062d6627193e94cf04d03c228dd64fd239
Nicholas Heo <nicholas.heo95@gmail.com>
2020-03-27 14:01:50 -0400

    changes

--- client/src/components/CommentForm.jsx
+++ client/src/components/CommentForm.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import styled from 'styled-components';
-import $ from 'jquery';
-import axios from 'axios';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import $ from "jquery";
+import axios from "axios";
 
 const CommentFormBlock = styled.div`
   width: 100vw;
   max-width: 850px;
   height: 40px;
-  font: 12px/1.4 Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;
+  font: 12px/1.4 Lucida Grande, Lucida Sans Unicode, Lucida Sans, Garuda,
+    Verdana, Tahoma, sans-serif;
   background-color: #f2f2f2;
   border: 1px solid #e5e5e5;
   color: #333;
@@ -24,7 +24,7 @@ const CommentFormBlock = styled.div`
   position: absolute;
   top: -120px;
   left: -140px;
-`
+`;
 
 const UserAvatar = styled.div`
   width: 40px;
@@ -32,27 +32,28 @@ const UserAvatar = styled.div`
   background-position: 50% 50%;
   background-size: cover;
   background-image: url("https://fec-comments-images.s3.us-east-2.amazonaws.com/zelda.jpg");
-`
+`;
 
 const CommentInput = styled.input`
   border-radius: 4px;
   background: #fff;
   font-size: 14px;
-  font-family: Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;
+  font-family: Lucida Grande, Lucida Sans Unicode, Lucida Sans, Garuda, Verdana,
+    Tahoma, sans-serif;
   outline: none;
   cursor: pointer;
   border: 1px solid #e5e5e5;
   width: 100%;
   margin: 7px 7px;
   padding-left: 9px;
-`
+`;
 
 class CommentForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      commentInput: ''
-    }
+      commentInput: ""
+    };
     this.handleInput = this.handleInput.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
@@ -60,38 +61,45 @@ class CommentForm extends React.Component {
   handleInput(e) {
     this.setState({
       commentInput: e.target.value
-    })
+    });
 
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       this.setState({
-        commentInput: ''
-      })
-      this.handleSubmit(e)
+        commentInput: ""
+      });
+      this.handleSubmit(e);
     }
   }
 
   handleSubmit(e) {
     e.preventDefault();
 
-      let newComment = {
-        user_name: 'ZeldaXOXO',
-        text: e.target.value
-      }
-      axios.post('/comments', newComment )
-        .then((result) => {
-          this.props.getComments();
-        })
-        .catch((err) => {
-          console.log('Error: Post comment:', err)
-        })
-
+    let newComment = {
+      user_name: "ZeldaXOXO",
+      text: e.target.value
+    };
+    axios
+      .post("http://localhost:3005/comments", newComment)
+      .then(result => {
+        this.props.getComments();
+      })
+      .catch(err => {
+        console.log("Error: Post comment:", err);
+      });
   }
 
   render() {
     return (
       <CommentFormBlock>
-        <UserAvatar /><CommentInput placeholder="Write a comment" value={ this.state.commentInput } onChange={ this.handleInput } onKeyPress={ this.handleInput } />
-      </CommentFormBlock>)
+        <UserAvatar />
+        <CommentInput
+          placeholder="Write a comment"
+          value={this.state.commentInput}
+          onChange={this.handleInput}
+          onKeyPress={this.handleInput}
+        />
+      </CommentFormBlock>
+    );
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-const db = require('./db');
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const $ = require('jquery');
-const router = require('./router.js');
+const db = require("./db");
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const $ = require("jquery");
+const router = require("./router.js");
 
 let app = express();
 
 // Serve up static html
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
 // Router
-app.use('/', router);
+app.use("/", router);
 
 module.exports = app;
